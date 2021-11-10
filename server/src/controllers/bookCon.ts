0c5cd9e951536ae8a95c7a912eb327b59a35bbeb
Craig <indigomx9@gmail.com>
2021-11-09 22:01:05 -0500

    Added the Update and Delete functions.

--- server/src/controllers/bookCon.ts
+++ server/src/controllers/bookCon.ts
@@ -30,7 +30,47 @@ async (req, res, next) => {
     }
 };
 
+export const GetOneBook: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const book: Books = 
+            await Books.findOneOrFail(req.params.id);
+        return res.status(201).json(book);
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
+export const UpdateBook: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const book: Books = 
+            await Books.findOneOrFail(req.params.id);
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.email = req.body.email;
+        book.age = req.body.age;
+        book.info = req.body.info;
+        await book.save();
+        return res.status(201).json("The Book was Updated!");
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
+export const DeleteBook: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const book: Books = 
+            await Books.findOneOrFail(req.params.id);
+        await book.remove();
+        res.status(201).json("The Book was Deleted!");
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
 
--- server/src/routes/bookRt.ts
+++ server/src/routes/bookRt.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { CreateBook, 
-    FetchAllBooks } from "../controllers/bookCon";
+import { CreateBook, FetchAllBooks, GetOneBook, 
+    UpdateBook, DeleteBook } from "../controllers/bookCon";
 
 export const bookRt: express.Router = express.Router();
     bookRt.post("/create", CreateBook);
     bookRt.get("/books", FetchAllBooks);
-
+    bookRt.get("/books/:id", GetOneBook);
+    bookRt.put("/books/:id", UpdateBook);
+    bookRt.delete("/books/:id", DeleteBook);
 
 
 
