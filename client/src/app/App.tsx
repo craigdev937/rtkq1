d3a4fd3218c61aab750d82d5da612693b9cd0926
Craig <indigomx9@gmail.com>
2021-11-10 01:13:54 -0500

    Initial setup of the React front-end.

---
+++ client/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sint aliquam officiis obcaecati inventore, neque eaque dolor libero in laudantium, nisi provident, doloribus vel veritatis perspiciatis excepturi velit rerum beatae?</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
